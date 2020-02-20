21cdfa62e4e40a79919317c25e8a86ca3e64aeb9
Christian Nilsendahl <christian.nilsendahl@avalanchestudios.se>
2020-02-20 11:59:24 +0100

    Updated licence headers

--- RenderFromACameraSwitcher.py
+++ RenderFromACameraSwitcher.py
@@ -1,4 +1,25 @@
-
+#
+# Copyright (c) 2020 Avalanche Studios. All rights reserved.
+# Licensed under the MIT License.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE
+#
 #--------------------------------------------------------------------------------------
 # File: RenderFromACameraSwitcher.py
 #
@@ -15,9 +36,6 @@
 # * in public_common.py you can find general pipeline function for sanitize relative path to your project
 #      and a function to add render files to your version control system
 #  Customize public_common functions depends on your needs.
-#
-# Copyright (c) Avalanche Studios. All rights reserved.
-# Licensed under the MIT License.
 #-------------------------------------------------------------------------------------
 
 import os
